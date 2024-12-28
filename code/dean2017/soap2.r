8d1e6c281319b8249ca2aaafd46e8f0662adbaa0
quantpsych <dtofighi@gmail.com>
2024-12-27 20:23:10 -0800

    Built site for gh-pages

---
+++ code/dean2017/soap2.r
@@ -0,0 +1,7 @@
+# soap2.r, soap experiment, Table 3.12, page 64
+
+# install.packages("pwr")
+library(pwr)
+v = 3; del = 0.25; sig2 = 0.007; alpha = 0.05; pwr = 0.90
+pwr.anova.test(k = v, sig.level = alpha, power = pwr,
+               f = sqrt(del^2/(2*v*sig2)))
